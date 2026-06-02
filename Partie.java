822a7061ff0849c445b99772234acd5d85f7c2c2
Zachary <zacharydiarra@gmail.com>
2026-06-02 16:51:58 +0200

    test

---
+++ Partie.java
@@ -0,0 +1,31 @@
+public class Partie 
+{
+	private int nombreManche;
+
+	
+	public Partie()
+	{
+
+	}
+
+	public void demarrer()
+	{
+
+	}
+
+	public Manche nouvelleManche()
+	{
+
+	}
+
+	public boolean estTerminee()
+	{
+
+	}
+
+	public Joueur getGagnant()
+	{
+
+	}
+}
+
