dc8e62068d80e21b8d5e88808da33fb697a8059c
maleknacef42-lab <maleknacef42@gmail.com>
2026-06-10 11:46:02 +0200

    Lien ihm metier + images de carte qui marche ne cliquant + pioche aleatoire fonctionnel

---
+++ jeu/Controleur.java
@@ -0,0 +1,23 @@
+package jeu;
+import jeu.ihm.FramePrincipale;
+
+public class Controleur
+{
+
+	private FramePrincipale ihm;
+	
+
+	public Controleur()
+	{
+		this.ihm = new FramePrincipale ( this );
+	}
+
+
+
+
+
+	public static void main (String[] args)
+	{
+		new Controleur();
+	}
+}
---
+++ jeu/ihm/FramePrincipale.java
@@ -0,0 +1,70 @@
+package jeu.ihm;
+import javax.swing.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.CardLayout;
+import jeu.Controleur; 
+
+
+public class FramePrincipale extends JFrame
+{
+	private PanelAccueil panelAccueil;
+	private PanelRegle   panelRegle  ;
+	private PanelSolo    panelSolo   ; 
+	private PanelDuo     panelDuo     ; 
+	private Controleur   ctrl        ;
+
+	private int          hauteur     ;
+	private int          largeur     ;
+
+	private CardLayout   cardLayout  ;
+
+	private JPanel       panelPrincipal;
+
+	public FramePrincipale ( Controleur ctrl)
+	{
+
+		Dimension tailleEcran = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+		this.hauteur = (int) ( tailleEcran.getHeight() - 45);
+		this.largeur = (int) ( tailleEcran.getWidth ()     );
+		
+		
+		this.setTitle   ("Orientis");
+		this.setLocation( 0, 0);
+		this.setSize    ( this.getDimensionPanel() );
+
+		this.ctrl = ctrl;
+
+		this.cardLayout = new CardLayout();
+		this.panelPrincipal = new JPanel ( this.cardLayout );
+		
+		this.panelAccueil = new PanelAccueil( this );
+		this.panelRegle   = new PanelRegle  ( this );
+		this.panelSolo    = new PanelSolo   ( this );
+		this.panelDuo     = new PanelDuo	( this ); 
+
+		//ajout des panel avec le cardLayout
+
+		this.panelPrincipal.add( this.panelAccueil, "accueil");
+		this.panelPrincipal.add( this.panelRegle  , "regle"  );
+		this.panelPrincipal.add( this.panelSolo,    "solo");
+		this.panelPrincipal.add( this.panelDuo,     "duo") ; 
+
+		this.cardLayout.show ( this.panelPrincipal, "accueil");
+
+
+		this.add( this.panelPrincipal);
+		
+		this.setVisible(true);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+
+	public Dimension getDimensionPanel()  { return new Dimension ( this.largeur, this.hauteur ); }
+
+	public void switchPanel ( String nom )
+	{
+		this.cardLayout.show ( this.panelPrincipal, nom );
+	}
+	
+}
